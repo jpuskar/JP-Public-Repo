c49fd0f09daafaea86e2cba5d8937800a96cdc69
John Puskar <johnpuskar@gmail.com>
2014-04-21 23:04:21 -0400

    Get-InstalledPrograms-via-dotnet4 now checks for dotnet4

--- SCCM/Package Scripts/Get-InstalledPrograms/Get-InstalledPrograms-via-dotnet4.ps1
+++ SCCM/Package Scripts/Get-InstalledPrograms/Get-InstalledPrograms-via-dotnet4.ps1
@@ -1,37 +1,59 @@
-$hive64 = $null
-$hive64 = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
-$rootKey64 = $null
-$rootKey64 = $hive64.OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall")
-$subKeyList64 = $null
-$subKeyList64 = $rootKey64.GetSubkeyNames()
+#reads 64bit and 32bit reg keys on 64-bit systems
+#this script requires .NET 4.0
+#http://stackoverflow.com/questions/630382/how-to-access-the-64-bit-registry-from-a-32-bit-powershell-instance
+
+$CLRVer = $null
+$CLRVer = [environment]::Version
+If($CLRVer.Major -lt 4) {
+	Write-Host -f red "This script requires .Net CLR 4 or above. Please install WMF 3+"
+	Exit
+}
+
+$regStrings = $null
+$regStrings = @()
+$regStrings += "SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+$regStrings += "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+
+$hive = $null
+$hive = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
 
 $progs = $null
 $progs = @()
 
-$subKeyList64 | % {
-	$subkey = $null
-	$subkey = $RootKey64.OpenSubKey($_, $false)
+$regStrings | % {
+	$rootKey = $null
+	$rootKey = $hive.OpenSubKey($_)
 	
-	$displayName = $null
-	$displayName = $subkey.GetValue("DisplayName")
-	
-	If($displayName -ne $null -and $displayName -ne "") {
-		$uninstallString = $null
-		$uninstallString = $subkey.GetValue("UninstallString")
+	If($rootKey -ne $null -and $rootKey -ne "") {
+		$subKeyList = $null
+		$subKeyList = $rootKey.GetSubkeyNames()
 		
-		$quietUninstallString = $null
-		$quietUninstallString = $subkey.GetValue("QuietUninstallString")
-		
-		$displayVersion = $null
-		$displayVersion = $subkey.GetValue("DisplayVersion")
-		
-		$progs += New-Object psObject -Property @{
-			"DisplayName" = $displayName;
-			"DisplayVersion" = $displayVersion;
-			"UninstallString" = $uninstallString;
-			"QuietUninstallString" = $quietUninstallString
-		}
-	}
-}
+		$subKeyList | % {
+			$subkey = $null
+			$subkey = $RootKey.OpenSubKey($_, $false)
+			
+			$displayName = $null
+			$displayName = $subkey.GetValue("DisplayName")
+			
+			If($displayName -ne $null -and $displayName -ne "") {
+				$uninstallString = $null
+				$uninstallString = $subkey.GetValue("UninstallString")
+				
+				$quietUninstallString = $null
+				$quietUninstallString = $subkey.GetValue("QuietUninstallString")
+				
+				$displayVersion = $null
+				$displayVersion = $subkey.GetValue("DisplayVersion")
+				
+				$progs += New-Object psObject -Property @{
+					"DisplayName" = $displayName;
+					"DisplayVersion" = $displayVersion;
+					"UninstallString" = $uninstallString;
+					"QuietUninstallString" = $quietUninstallString
+				} #progs object
+			} #grabs progs properties
+		} #subkeyList
+	} #rootKey
+} #keys to search
 
-#$progs | sort displayName | format-table -property displayname,displayversion,uninstallstring,quietuninstallstring
+$progs
